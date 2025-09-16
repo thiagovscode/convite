261a5b9342bc3cd0854874d56397d37cf895f8a2
thiagovscode <southiagovasconcelos@gmail.com>
2025-09-15 23:21:19 -0300

    editando

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Altere `convite` para o nome do seu repositório no GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/convite/',
+  base: '/convite',
 })
